f9c614a5d71569a0d112815a1d7b793fd5f02084
Daniel Brandão <drb12da@gmail.com>
2022-03-30 17:57:04 -0300

    novo botao

--- index.js
+++ index.js
@@ -4,31 +4,36 @@ var adicionar = document.getElementById('add');
 var sorteador = document.getElementById('sortear');
 var nomeSorteado = document.getElementById('sorteado');
 var tabela = document.getElementById('table')
-
+var novaLista = document.getElementById('novaLista');
+var display = document.getElementById('load').style.display;
 let array = [];
 
 
 document.getElementById('load').style.display = 'none'
-    
+  novaLista.style.display ='none'
+  
+
+    function Mudarestado() {
+        
+        if(display == "none")
+        document.getElementById('load').style.display = 'block'
+        else
+        document.getElementById('load').style.display = 'none'
+    }
 
 
 adicionar.addEventListener("click",e=>{
-   
-  let names = nome.value;
- 
+    let names = nome.value;
   if (names === ""){
-   
     alert("Preecha o campo")
- 
   }else{
 
-      if(array.includes(names)){
+    
         
+      if(array.includes(names)){
         alert('Este nome já existe na lista')
         nome.value='';
-        
       }else{
-        
         array.push(names)
         let tr = document.createElement('tr');
       
@@ -49,23 +54,33 @@ adicionar.addEventListener("click",e=>{
   
 
 sorteador.addEventListener('click', e=>{
-  
   if(array.length <= 0){
-    
     alert('Lista vazia')
-  
   }else{
-   
     document.getElementById('load').style.display = 'block'
 
+    
     setTimeout(  function(){  
         const sorteado =Math.floor(Math.random() * array.length);
         let tr = document.createElement('tr');
         tr.innerHTML= `<td>Parabéns ${array[sorteado]}</td>`
         this.nomeSorteado.appendChild(tr)
         document.getElementById('load').style.display = 'none'
-        
+        adicionar.disabled = true;
+        sorteador.disabled = true;
+        novaLista.style.display ='block'
     },1000)
+    
+    
+  
   }
+});
+
+novaLista.addEventListener('click', e=>{
+
+
+    document.location.reload(true)
+  
+      
 });
 
